cc0d4a3253d28a1c13ac70e874575a376e07b5c0
wuchenyv1990 <350200039@qq.com>
2019-10-28 21:13:20 +0800

    rename recApp

--- spring-demo/rabbitmq-boot/rabbitmq-receiver/src/main/java/com/wuchenyv1990/rabbitmq/rec/RecApplication.java
+++ spring-demo/rabbitmq-boot/rabbitmq-receiver/src/main/java/com/wuchenyv1990/rabbitmq/rec/RecApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableRabbit
-public class RecAppplication {
+public class RecApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RecAppplication.class, args);
+        SpringApplication.run(RecApplication.class, args);
     }
 
 }
